cb2fbc8482c03bda4d652cfd3d8dcd7281249006
chilpikdev <ihaliev@yandex.ru>
2024-10-18 00:51:59 +0500

    ref: changed statistics resource

--- app/Http/Resources/Statistics/IndexResource.php
+++ app/Http/Resources/Statistics/IndexResource.php
@@ -18,24 +18,28 @@ class IndexResource extends JsonResource
             'name' => $this->name,
             'points' => $this->points,
             'completed' => [
-                'on_time' => [
-                    'high' => $this->completed_on_time_high,
-                    'medium' => $this->completed_on_time_medium,
-                ],
-                'expired' => [
-                    'high' => $this->completed_expired_high,
-                    'medium' => $this->completed_expired_medium,
-                ],
+                'on_time' => $this->completed_on_time_high + $this->completed_on_time_medium,
+                // 'on_time' => [
+                //     'high' => $this->completed_on_time_high,
+                //     'medium' => $this->completed_on_time_medium,
+                // ],
+                'expired' => $this->completed_expired_high + $this->completed_expired_medium,
+                // 'expired' => [
+                //     'high' => $this->completed_expired_high,
+                //     'medium' => $this->completed_expired_medium,
+                // ],
             ],
             'unfulfilled' => [
-                'active' => [
-                    'high' => $this->unfulfilled_active_high,
-                    'medium' => $this->unfulfilled_active_medium
-                ],
-                'expired' => [
-                    'high' => $this->unfulfilled_expired_high,
-                    'medium' => $this->unfulfilled_expired_medium
-                ],
+                'active' => $this->unfulfilled_active_high + $this->unfulfilled_active_medium,
+                // 'active' => [
+                //     'high' => $this->unfulfilled_active_high,
+                //     'medium' => $this->unfulfilled_active_medium
+                // ],
+                'expired' => $this->unfulfilled_expired_high + $this->unfulfilled_expired_medium,
+                // 'expired' => [
+                //     'high' => $this->unfulfilled_expired_high,
+                //     'medium' => $this->unfulfilled_expired_medium
+                // ],
             ]
         ];
     }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         ]);
         $middleware->appendToGroup('api', [
             \App\Http\Middleware\ChangeLocale::class,
